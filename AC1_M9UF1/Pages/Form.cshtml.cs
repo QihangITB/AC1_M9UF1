073c7f8cc8a1191a09c89c33d18700900e003cbf
argo <argo@MAQUETA>
2024-04-29 13:22:35 +0200

    Add project files.

---
+++ AC1_M9UF1/Pages/Form.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AC1_M9UF1.Pages
+{
+    public class FormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
