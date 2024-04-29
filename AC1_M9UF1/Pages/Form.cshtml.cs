202d83e1be8c56059a98794873ef1dc7eee0100f
qihang <qihang@pc.com>
2024-04-29 13:56:52 +0200

    first commit

--- AC1_M9UF1/Pages/Form.cshtml.cs
+++ AC1_M9UF1/Pages/Form.cshtml.cs
@@ -5,8 +5,21 @@ namespace AC1_M9UF1.Pages
 {
     public class FormModel : PageModel
     {
+        [BindProperty]
+        public bool IsPost { get; set; }
+
+        public FormModel()
+        {
+            IsPost = false;
+        }
+
+        public void OnPost()
+        {
+            IsPost = true;
+        }
         public void OnGet()
         {
+
         }
     }
 }
